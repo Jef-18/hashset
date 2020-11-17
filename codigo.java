05fbb5affc0309cd90b3518ddcce1937f5743fe0
Jef-18 <37846691+Jef-18@users.noreply.github.com>
2020-11-17 20:35:19 -0300

    Create codigo.java
    Evitando valores com duplicidade.

---
+++ codigo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+class Main {
+
+  public static void main(String[] args) {
+    Set<String> nomes = new HashSet<>();
+    nomes.add(" Homem-Aranha ");
+    nomes.add(" Super-Homem ");
+    nomes.add(" Super Mario ");
+    nomes.add(" Super Mario ");
+
+    System.out.println(nomes);
+    System.out.println(nomes.size());
+
+  }
+}
